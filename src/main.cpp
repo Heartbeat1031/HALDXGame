36e7ea4a8bc5bdc2510b8a82e7b71042fd48607d
lclcl <lclclcc777@gmail.com>
2025-05-08 23:58:25 +0900

    プロジェクト作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 int main()
 {
+	SetConsoleOutputCP(65001);
 	std::cout << "Hello World." << std::endl;
+	std::cout << "你好 世界。" << std::endl;
+	std::cout << "こんにちは 世界。" << std::endl;
 	return 0;
 }
