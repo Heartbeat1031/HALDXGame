2762aabc554413d0759fd42c03fdc533aac5eed7
lclclcc777 <lclclcc777@gmail.com>
2025-05-09 01:16:18 +0900

    // モジュールの追加テスト

---
+++ src/modules/GameFramework/GameFramework.cpp
@@ -0,0 +1,15 @@
+//
+// Created by clcc777 on 25-5-9.
+//
+
+#include "GameFramework.h"
+#include <iostream>
+
+GameFramework::GameFramework() = default;
+
+void GameFramework::Hello() {
+    std::cout << "Hello from GameBase!" << std::endl;
+}
+
+
+
---
+++ src/modules/GameFramework/GameFramework.h
@@ -0,0 +1,10 @@
+//
+// Created by clcc777 on 25-5-9.
+//
+#pragma once
+
+class GameFramework {
+public:
+    GameFramework();
+    static void Hello();
+};
