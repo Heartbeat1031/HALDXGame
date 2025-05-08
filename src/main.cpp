44ceec131463bb9bb376709ab9e2be5d87dfb384
lclclcc777 <lclclcc777@gmail.com>
2025-05-09 01:16:09 +0900

    // モジュールの追加テスト

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include "GameFramework.h"
 
 int main()
 {
 	// UTF-8を使用するための設定
 	SetConsoleOutputCP(65001);
+
 	std::cout << "Hello World." << std::endl;
 	std::cout << "こんにちは 世界。" << std::endl;
 	std::cout << "你好 世界。" << std::endl;
+	GameFramework::Hello();
 	return 0;
 }
