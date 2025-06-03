17a9667cc967c0c4f5be9f36744582e0dc45a656
lclcl <lclclcc777@gmail.com>
2025-06-03 23:33:56 +0900

    初步添加物理

---
+++ src/base/JoltListener.cpp
@@ -0,0 +1,35 @@
+﻿#include "JoltListener.h"
+#include <iostream>
+
+using namespace std;
+
+// ValidateResult JoltContactListener::OnContactValidate(const Body &inBody1, const Body &inBody2, RVec3Arg inBaseOffset, const CollideShapeResult &inCollisionResult)
+// {
+//     cout << "Contact validate callback" << endl;
+//     return ValidateResult::AcceptAllContactsForThisBodyPair;
+// }
+//
+// void JoltContactListener::OnContactAdded(const Body &inBody1, const Body &inBody2, const ContactManifold &inManifold, ContactSettings &ioSettings)
+// {
+//     cout << "A contact was added" << endl;
+// }
+//
+// void JoltContactListener::OnContactPersisted(const Body &inBody1, const Body &inBody2, const ContactManifold &inManifold, ContactSettings &ioSettings)
+// {
+//     cout << "A contact was persisted" << endl;
+// }
+//
+// void JoltContactListener::OnContactRemoved(const SubShapeIDPair &inSubShapePair)
+// {
+//     cout << "A contact was removed" << endl;
+// }
+//
+// void JoltBodyActivationListener::OnBodyActivated(const BodyID &inBodyID, JPH::uint64 inBodyUserData)
+// {
+//     cout << "A body got activated" << endl;
+// }
+//
+// void JoltBodyActivationListener::OnBodyDeactivated(const BodyID &inBodyID, JPH::uint64 inBodyUserData)
+// {
+//     cout << "A body went to sleep" << endl;
+// }
---
+++ src/base/JoltListener.h
@@ -0,0 +1,28 @@
+﻿// JoltListener.h
+#pragma once
+
+#include <Jolt/Physics/Collision/ContactListener.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+#include <Jolt/Math/Real.h>
+
+#include "Jolt/Core/Core.h"
+
+class JoltContactListener : public ContactListener
+{
+public:
+    virtual ValidateResult OnContactValidate(const Body &inBody1, const Body &inBody2, RVec3Arg inBaseOffset, const CollideShapeResult &inCollisionResult) override;
+
+    virtual void OnContactAdded(const Body &inBody1, const Body &inBody2, const ContactManifold &inManifold, ContactSettings &ioSettings) override;
+
+    virtual void OnContactPersisted(const Body &inBody1, const Body &inBody2, const ContactManifold &inManifold, ContactSettings &ioSettings) override;
+
+    virtual void OnContactRemoved(const SubShapeIDPair &inSubShapePair) override;
+};
+
+class JoltBodyActivationListener : public BodyActivationListener
+{
+public:
+    virtual void OnBodyActivated(const BodyID &inBodyID, JPH::uint64 inBodyUserData) override;
+
+    virtual void OnBodyDeactivated(const BodyID &inBodyID, JPH::uint64 inBodyUserData) override;
+};
