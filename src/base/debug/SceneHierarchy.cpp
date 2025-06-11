b0a4fb41bac195d1fbee41fa08e8591aae775363
周凱 OHS24 <ohs40570@ohs.hal.ac.jp>
2025-06-11 11:27:57 +0900

    DrawInspector更新

--- src/base/debug/SceneHierarchy.cpp
+++ src/base/debug/SceneHierarchy.cpp
@@ -55,18 +55,72 @@ void SceneHierarchy::DrawSceneNode(const UID rootid, UID &selectedid) {
     }
 }
 
+//void SceneHierarchy::DrawInspector(UID selectedid) {
+//    if (selectedid == -1) {
+//        ImGui::Text("No Object Selected");
+//        return;
+//    }
+//
+//    GameObject *go = halgame->GetScene()->GetGameObject<GameObject>(selectedid);
+//    if (!go) {
+//        ImGui::Text("GameObject not found.");
+//        return;
+//    }
+//
+//    ImGui::Text("名前 : %s (%d)", go->GetName().c_str(), selectedid);
+//
+//    // ------ Transform ------
+//    TransformC &tf = go->GetComponentRef<TransformC>();
+//    ImGui::Text("Transform");
+//
+//    ImGui::DragFloat3("Position", &tf.position.x, 0.1f);
+//    ImGui::DragFloat3("Rotation", &tf.rotation.x, 0.5f);
+//    ImGui::DragFloat3("Scale",    &tf.scale.x,    0.05f);
+//}
+
 void SceneHierarchy::DrawInspector(UID selectedid) {
     if (selectedid == -1) {
         ImGui::Text("No Object Selected");
         return;
     }
-    GameObject *go = halgame->GetScene()->GetGameObject<GameObject>(selectedid);
+
+    GameObject* go = halgame->GetScene()->GetGameObject<GameObject>(selectedid);
     if (!go) {
         ImGui::Text("GameObject not found.");
         return;
     }
+
     ImGui::Text("名前 : %s (%d)", go->GetName().c_str(), selectedid);
+
     // ------ Transform ------
-    TransformC &tf = go->GetComponentRef<TransformC>();
-    tf.OnInspectorGUI();
+    TransformC& tf = go->GetComponentRef<TransformC>();
+    ImGui::Text("Transform");
+
+    float pos[3] = {
+        tf.GetLocalPosition().x,
+        tf.GetLocalPosition().y,
+        tf.GetLocalPosition().z
+    };
+    float rot[3] = {
+        tf.GetLocalRotationEuler().x,
+        tf.GetLocalRotationEuler().y,
+        tf.GetLocalRotationEuler().z
+    };
+    float scale[3] = {
+        tf.GetLocalScale().x,
+        tf.GetLocalScale().y,
+        tf.GetLocalScale().z
+    };
+
+    if (ImGui::DragFloat3("Position", pos, 0.1f)) {
+        tf.SetLocalPosition({ pos[0], pos[1], pos[2] });
+    }
+
+    if (ImGui::DragFloat3("Rotation", rot, 0.5f)) {
+        tf.SetLocalRotationEuler({ rot[0], rot[1], rot[2] });
+    }
+
+    if (ImGui::DragFloat3("Scale", scale, 0.05f)) {
+        tf.SetLocalScale({ scale[0], scale[1], scale[2] });
+    }
 }
