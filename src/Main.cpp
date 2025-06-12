33ac867d5c860c03cc5ac977eb63f3f74a55c3d6
lclcl <lclclcc777@gmail.com>
2025-06-12 14:52:00 +0900

    SceneHierarchyを追加する

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,7 +15,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
     // ゲームインスタンスを作成します
-    halgame = new Game(hInstance, L"Meshes", 1280, 720);
+    halgame = new Game(hInstance, L"HalGame", 1280, 720);
     // ゲームインスタンスを初期化します
     if( !halgame->Init() ) {
         return 0;
