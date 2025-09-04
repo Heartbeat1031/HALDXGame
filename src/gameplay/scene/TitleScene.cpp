c237715eab04651ebab6079700514513d4868f3c
李成林 <lclclcc777@gmail.com>
2025-09-04 17:17:05 +0900

    Update TitleScene to adjust camera settings and scale background model

--- src/gameplay/scene/TitleScene.cpp
+++ src/gameplay/scene/TitleScene.cpp
@@ -8,29 +8,14 @@ void TitleScene::Init() {
     auto& screen = AddGameObject<GameObject>();
     screen.AddComponent<ModelC>("assets/models/bg.fbx");
     screen.GetComponentRef<TransformC>().SetWorldPosition(Vector3(0, 0, 0));
-    // screen.AddComponent<BoxCollisionC>(
-    //     JPH::EMotionType::Static, // 静的な物体
-    //     Vector3(10, 1, 10) // サイズを設定します
-    // );
-    // screen.GetComponentRef<BoxCollisionC>().SetOffsetTransform(
-    //     OffsetTransform{
-    //         Vector3(0, -2, 0), // 地面の位置オフセット
-    //         Vector3(0, -90, 0)   // 回転オフセット
-    //     }
-    // );
+    screen.GetComponentRef<TransformC>().SetLocalScale(Vector3(3.8f, 3.8f, 1));
+
+    halgame->m_pCamera->SetTarget(Vector3(0, 0, 0));
+    halgame->m_pCamera->Approach(-10);
+    halgame->m_pCamera->SetRotationX(-1);
 }
 
 void TitleScene::Update() {
-    // 三人称カメラの操作
-    ImGuiIO& io = ImGui::GetIO();
-    // カメラのターゲットをミクの位置に設定します
-    halgame->m_pCamera->SetTarget(Vector3(0, 0, 1));
-    // オブジェクトの周りを回転する
-    if (ImGui::IsMouseDragging(ImGuiMouseButton_Right)) {
-        halgame->m_pCamera->RotateX(io.MouseDelta.y * 0.01f);
-        halgame->m_pCamera->RotateY(io.MouseDelta.x * 0.01f);
-    }
-    halgame->m_pCamera->Approach(-io.MouseWheel * 1.0f);
 }
 
 void TitleScene::Uninit() {
